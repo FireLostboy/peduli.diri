1b78e003849b7435dffbe78c3e1362b18e5820d8
FireLostboy <101849011+FireLostboy@users.noreply.github.com>
2022-04-07 10:55:46 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('nik')->nullable();
             $table->string('name')->nullable();
             $table->string('telp');
+            $tabel->string('alamat')->nullable();
             $table->string('email')->unique();
             $table->string('foto')->nullable();
             $table->timestamp('email_verified_at')->nullable();
